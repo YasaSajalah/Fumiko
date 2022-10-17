adc4f15f4430a475195622c3e05e84a3a4c40b06
YasaSajalah <112755403+YasaSajalah@users.noreply.github.com>
2022-10-17 23:01:50 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,19 +2,9 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtu.be/O91DT1pR1ew*
+let str = `*FumikoBotz*
 
-Pengen mentahan nya ?
-Nih ada santai bro....
-https://youtu.be/O91DT1pR1ew
-
-Mau Yang No Error Script Bot nya, Gas Cek Video YT :
-https://youtu.be/O91DT1pR1ew
-
-Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
-
-wa.me/6281218100935`
+*Tidak Ada Yang Gratis*`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
